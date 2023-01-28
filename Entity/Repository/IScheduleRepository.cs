2820bbbd249301f224d62f9db858cc9addd4ae2a
Faner201 <62674880+Faner201@users.noreply.github.com>
2023-01-28 19:18:26 +1000

    Creating asynchronous data processing for the schedule

--- Entity/Repository/IScheduleRepository.cs
+++ Entity/Repository/IScheduleRepository.cs
@@ -1,7 +1,7 @@
 namespace Entity;
 public interface IScheduleRepository
 {
-   Schedule? GetDoctorScheduleByDate(int doctorID, DateTime date);
-   Schedule? AddScheduleDoctor(Schedule schedule);
-   Schedule? EditScheduleDoctor(Schedule actual, Schedule recent);
+   Task<Schedule?> GetDoctorScheduleByDate(int doctorID, DateTime date);
+   Task<Schedule?> AddScheduleDoctor(Schedule schedule);
+   Task<Schedule?> EditScheduleDoctor(Schedule actual, Schedule recent);
 }
--- Entity/ScheduleService.cs
+++ Entity/ScheduleService.cs
@@ -2,27 +2,57 @@ namespace Entity;
 public class ScheduleService
 {
     private IScheduleRepository _scheduleService;
-
+    private static SemaphoreSlim scheduleSemaphore = new SemaphoreSlim(1, 1);
     public ScheduleService(IScheduleRepository scheduleService)
     {
         _scheduleService = scheduleService;
     }
 
-    public Result<Schedule> GetDoctorScheduleByDate(int doctorID, DateTime date)
+    async public Task<Result<Schedule>> GetDoctorScheduleByDate(int doctorID, DateTime date)
     {
-        var request = _scheduleService.GetDoctorScheduleByDate(doctorID, date);
+        Schedule? request = null;
+        try
+        {
+            await scheduleSemaphore.WaitAsync();
+
+            request = await _scheduleService.GetDoctorScheduleByDate(doctorID, date);
+        }
+        finally
+        {
+            scheduleSemaphore.Release();
+        }
 
         return request is null ? Result.Fail<Schedule>("The schedule for this doctor was not found ") : Result.Ok<Schedule>(request);
     }
-    public Result<Schedule> AddScheduleDoctor(Schedule schedule)
+    async public Task<Result<Schedule>> AddScheduleDoctor(Schedule schedule)
     {
-        var request = _scheduleService.AddScheduleDoctor(schedule);
+        Schedule? request = null;
+        try
+        {
+            await scheduleSemaphore.WaitAsync();
+
+            request = await _scheduleService.AddScheduleDoctor(schedule);
+        }
+        finally
+        {
+            scheduleSemaphore.Release();
+        }
 
         return request is null ? Result.Fail<Schedule>("I couldn't add the schedule") : Result.Ok<Schedule>(request);
     }
-    public Result<Schedule> EditScheduleDoctor(Schedule actual, Schedule recent)
+    async public Task<Result<Schedule>> EditScheduleDoctor(Schedule actual, Schedule recent)
     {
-        var request = _scheduleService.EditScheduleDoctor(actual, recent);
+        Schedule? request = null;
+        try
+        {
+            await scheduleSemaphore.WaitAsync();
+
+            request = await _scheduleService.EditScheduleDoctor(actual, recent);
+        }
+        finally
+        {
+            scheduleSemaphore.Release();
+        }
 
         return request is null ? Result.Fail<Schedule>("It was not possible to add changes to the schedule") : Result.Ok<Schedule>(request);
     }
